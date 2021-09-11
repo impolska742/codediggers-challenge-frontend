54760b9721cb544f5d8ea8ca7e28a36ee9757aa0
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-09-11 13:46:58 +0530

    Task 3 Elements Completed

---
+++ src/components/Task 3/Task3Elements.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const UserCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 500px;
+  padding: 40px 20px;
+`;
+
+export const UserName = styled.h1`
+  font-size: 1.5rem;
+  text-align: center;
+  color: blue;
+`;
+
+export const UserHandle = styled.h4`
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+export const UserCardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/components/Task 3/index.js
+++ src/components/Task 3/index.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  UserCard,
+  UserName,
+  UserHandle,
+  UserCardContainer,
+} from "./Task3Elements";
+import "./Task3.css";
 
 const Task3 = () => {
-  const [userData, setUserData] = useState();
+  const [userData, setUserData] = useState({});
+  const [contests, setContests] = useState([]);
 
-  const getUserData = () => {
-    axios
-      .get(
-        "https://api.codedigger.tech/auth/profile/shivamsinghal1012/?platform=codeforces"
-      )
-      .then(({ data }) => setUserData(data.result))
-      .catch((error) => console.log(error));
+  const getUserData = async () => {
+    const response = await axios.get(
+      "https://api.codedigger.tech/auth/profile/shivamsinghal1012/?platform=codeforces"
+    );
+
+    const data = await response.data.result;
+    setUserData(data);
+    setContests(data.contestRank);
   };
 
   useEffect(() => {
@@ -18,9 +27,33 @@ const Task3 = () => {
   }, []);
 
   return (
-    <div>
-      <h1>TASK 3</h1>
-    </div>
+    <UserCardContainer>
+      <UserCard className="user-card">
+        <UserName>{userData.name}</UserName>
+        <UserHandle>Codeforces : @{userData.handle}</UserHandle>
+        <p>Current Rating : {userData.rating}</p>
+        <p>Max Rating : {userData.maxRating}</p>
+        <p>Current Rank : {userData.rank}</p>
+        <p>Max Rank : {userData.maxRank}</p>
+        <p>World Rank : {userData.worldRank}</p>
+        <p>Country Rank : {userData.countryRank}</p>
+        <p>Organisation Rank : {userData.organizationRank}</p>
+        <div className="user-contests">
+          <h5>Contests :-</h5>
+          {contests.map((contestRank, idx) => {
+            const { contest } = contestRank;
+            return (
+              <div key={idx} className="contest">
+                <p className="bold">Contest Name : {contest.name}</p>
+                <p>World Rank : {contestRank.worldRank}</p>
+                <p>Country Rank : {contestRank.countryRank}</p>
+                <p>Organization Rank : {contestRank.organizationRank}</p>
+              </div>
+            );
+          })}
+        </div>
+      </UserCard>
+    </UserCardContainer>
   );
 };
 
